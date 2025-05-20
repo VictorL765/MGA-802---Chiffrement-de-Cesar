20d446ab386b9211b3c60087ae21ddb0efec46f4
elsakupfer <elsa.kupfer.1@ens.etsmtl.ca>
2025-05-20 15:13:55 -0400

    hello

--- Code chifffement de Cesar.py
+++ Code chifffement de Cesar.py
@@ -1,3 +1,14 @@
 import string
 
 alphabet = string.ascii_lowercase
+
+
+
+# fonction qui renvoie l'index de chaque lettre du mot
+liste_index = []
+def index_mot(liste) :
+    for i in range (len(liste)) :
+         index = alphabet.find(liste[i])
+         liste_index.append(index)
+         print(liste_index)
+
