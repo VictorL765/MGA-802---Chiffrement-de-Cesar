a35674b913def47b3172cbf9b4aa27bd4d9accd6
elsakupfer <elsa.kupfer.1@ens.etsmtl.ca>
2025-05-20 16:30:29 -0400

    * indexation des caractère du mot

--- Code chifffement de Cesar.py
+++ Code chifffement de Cesar.py
@@ -1,19 +1,31 @@
+import string
+
+
+alphabet = string.ascii_lowercase
+liste_index = []
 
 def lecture_du_mot():
     mot = input('Entrez le mot : ')
     print(mot)
-        # Transforme la chaine de caracteres en liste
-        # le saut de ligne sert de separateur de champs
-    liste_de_caractere = mot.split('\n')
+    # Transforme la chaine de caracteres en liste
+    # le saut de ligne sert de separateur de champs
+    #liste_de_caractere = mot.split('\n')
 
     # retourne la liste de mots
-    return liste_de_caractere
+    return mot
+
 
 # fonction qui renvoie l'index de chaque lettre du mot
-liste_index = []
-def index_mot(liste) :
-    for i in range (len(liste)) :
-         index = alphabet.find(liste[i])
-         liste_index.append(index)
-         print(liste_index)
+def index_mot(mot) :
+    liste_index = []
+    for char in mot :
+        index = alphabet.index(char)
+        liste_index.append(index)
+        #index = alphabet.index(liste[i])
+    print(liste_index)
+
+mot = lecture_du_mot()
+index_mot(mot)
+
+
 
