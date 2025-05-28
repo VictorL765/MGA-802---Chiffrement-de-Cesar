1d4a8425d3d0e38820125286a235ff99b29596f9
Noé <noe.morance.1@ens.etsmtl.ca>
2025-05-28 19:01:36 -0400

    Implementation de la fonction enlever_caracteres_speciaux

--- Cryptage_decryptage.py
+++ Cryptage_decryptage.py
@@ -1,1 +1,12 @@
 #ce code sert à crypter/décrypter le teCryptagexte ou l'input d'entrée
+import string
+import unicodedata
+
+
+
+alphabet = string.ascii_lowercase
+
+#Pour retirer les accents on ce sert de l'outil necessaire page 8 du chiffrement de César
+def enlever_caracteres_speciaux(texte):
+    normalized_word=unicodedata.normalize('NFKD', texte)
+    return ''.join(char for char in normalized_word if not unicodadata.combining(char))
