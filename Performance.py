82f45b0a964ceda97b9df01ce1343cde3bb84741
VictorL765 <victor.laloi.1@ens.etsmtl.ca>
2025-05-30 19:16:27 -0400

    Création d'une fonction qui evalue le temps d'execution de notre focntion brute force

--- Performance.py
+++ Performance.py
@@ -1,7 +1,16 @@
 # Evaluation du temps de calul pour la fonction brute force
 
-from timeit import timeit
+from time import perf_counter
 from Bruteforce import brute_force
 
+#Fonction qui evalue le temps d'execution de la methode brute force
+def performance_brute_force(texte):
+    tic = perf_counter()  #Demarrage du chrono
+    brute_force(texte)
+    toc = perf_counter()  #Fin du chrono
 
-temps_performance = timeit(brute_force,
+    #Affichage du temps d'execution en seconde
+    print(f"temps d'execution de la fonction brute force : {toc-tic}  [s]")
+
+
+
