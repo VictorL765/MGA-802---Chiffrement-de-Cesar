7c74c40a597d9afc9f1f5de95f34b05b1235ce96
Noé <noe.morance.1@ens.etsmtl.ca>
2025-05-30 21:24:25 -0400

    importation de bruteforce dans interface

--- Bruteforce.py
+++ Bruteforce.py
@@ -2,79 +2,20 @@
 
 import string
 from Cryptage_decryptage import decrypter
-import os
+from FichierRessourse import lire_fichier
 
-#mots_courants = [
-   # "le", "la", "et", "un", "une", "est", "dans", "en", "il", "elle", "ce", "pour", "qui",
-   # "sur", "pas", "plus", "avec", "tout", "mais", "nous", "vous", "comme", "bien", "fait"
-#]
-
-alphabet = string.ascii_lowercase
-
-# Lis le fichier texte comportant les mots courants français et le fichier à décrypter
-def lire_texte(fichier="mots_courants.txt",texte="mots_pendu.txt"):
-
-    question = input('Voulez-vous utiliser votre propre fichier ? (oui/non) :')
-
-    if question=='oui' :
-        #Demande le nom du fichier à décrypter en format texte
-        fichier = str(input("Entrez le nom du fichier, n'oubliez pas .txt :"))
-
-    else :
-        print('ok')
-    lecture_fichier = fichier # la variable lescture_fichier prend le fichier des mots courants français
-    lecture_texte=texte # la variable lescture_texte prend le fichier par défaut à décrypter
-
-    # teste si le fichier existe
-    if not os.path.isfile(lecture_fichier):
-        raise RuntimeError(f'Je ne trouve pas le fichier {lecture_fichier} !')
-
-    # Ouvre le fichier contenant les mots en mode lecture
-    with open(lecture_fichier, 'r', encoding='utf8') as f:
-        # Lire le contenu du fichier
-        word1 = f.read()
-        words = word1.split() #sépare les chaines de caractère lorsqu'il y a un espace, permet d'identifier les mots
-
-
-    with open(lecture_texte, 'r', encoding='utf8') as f:
-        # Lire le contenu du fichier
-        texte_a_dechiffrer1=f.read()
-        texte_a_dechiffrer = texte_a_dechiffrer1.split()
-
-        # retourne la liste de mots
-        return words, texte_a_dechiffrer #retourne les fichiers comprenant les mots avec
-
-mots_courants, texte_a_decrypter=lire_texte()
-
-
-
-
-
-
-
-'''''
-def lire_texte():
-
-    lecture_fichier = input(r"Entrez le chemin du fichier texte :")
-
-    # teste si le fichier existe
-    if not os.path.isfile(lecture_fichier):
-        raise RuntimeError(f'Je ne trouve pas le fichier {lecture_fichier} !')
-
-    # Ouvre le fichier contenant les mots en mode lecture
-    with open(lecture_fichier, 'r', encoding='utf8') as f:
-        # Lire le contenu du fichier
-        words = f.read()
-
-        # retourne la liste de mots
-        return words
-
-lire_texte()
-
-'''''
-
+#charger des mots courants depuis un fichier txt
+def charger_mots_courants(fichier=#mots_courants.txt):
+    try:
+       contenu = lire_fichier(fichier)
+       mots_courants = contenu.split[]
+       return mots_courants
+    except Exception as e:
+       print(f"Erreur de chargarment des mots courants: {str(e)}")
+       return []
 
 def brute_force(texte):
+
     for cle in range(0, 25): #test les clefs possibles sur les lettres de l'alphabet
         texte_a_tester = decrypter(texte, cle)
         for mot in mots_courants:
@@ -95,5 +36,3 @@ def brute_force(texte):
 
     print("Aucun mot courant détecté.")
 
-texte_a_decrypter = ' '.join(texte_a_decrypter) #permet de transformé la variable liste, texte_a_decrypter en chaine de caractère
-brute_force(texte_a_decrypter) #execute le programme
